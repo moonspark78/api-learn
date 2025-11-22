2f80feb24e72781f7d05dba31623a9fccd855765
Omar Jabour <jabour.omar@hotmail.com>
2025-11-22 23:58:23 +0100

    :art: working on the apiPoke fetch

--- src/components/PokeApi.jsx
+++ src/components/PokeApi.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const PokeApi = () => {
     const [pokemon, setPokemon] = useState([])
 
+    useEffect(() =>{
+        const fetchData = async () => {
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+            const data = await response.json()
+            setPokemon(data.results)
+        }
+        fetchData()
+    }, [])
+
   return (
     <div>
         <h1>PokeApi</h1>
+        <ul>
+            {pokemon.map((poke, index) => (
+                <li key={index}>{poke.name}
+                    <img src={poke.image} alt={poke.name} width={100}/>
+                </li>
+            ))}
+        </ul>
     </div>
   )
 }
