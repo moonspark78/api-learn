ba5b950105fced950a2a605ca252d1e86f5e8f36
Omar Jabour <jabour.omar@hotmail.com>
2025-11-22 23:53:13 +0100

    :art: working on the apiPoke

---
+++ src/components/PokeApi.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const PokeApi = () => {
+    const [pokemon, setPokemon] = useState([])
+
+  return (
+    <div>
+        <h1>PokeApi</h1>
+    </div>
+  )
+}
+
+export default PokeApi
